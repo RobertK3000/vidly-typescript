2062fced7466f8b02edfc8a455e528a2e4f47cb8
RobertK3000 <robertcykwok@gmail.com>
2021-08-24 17:28:17 +1000

    first-commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import Movies from './components/movies';
+import './App.css';
+
+function App() {
+  return (
+    <main className="container">
+      <Movies/>
+    </main>
+  );
+}
+
+export default App;
